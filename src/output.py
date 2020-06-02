fce95514487ddb4e6b71b28d165cb0417c9f1b4c
Renke Kuhlmann <24522546+renkekuhlmann@users.noreply.github.com>
2020-06-02 11:19:56 +0200

    pylint

--- src/output.py
+++ src/output.py
@@ -19,7 +19,7 @@ class Output:
     """
     Formats and prints benchmark results
     """
-    # pylint: disable=too-few-public-methods
+    # pylint: disable=too-few-public-methods,too-many-instance-attributes
 
     def __init__(self):
         self.print_benchmark_meta = True
